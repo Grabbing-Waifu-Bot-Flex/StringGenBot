3d068ac0920c910f74d0ccde4db5763270aadf38
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-14 18:22:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "24089031"))
 API_HASH = getenv("API_HASH", "0615e3afe13ddaaf8e9ddbd3977d35ff")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7028288342:AAGO5QHFzY4jG0aRpnq3SF9r4IOpBe76qr0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7028288342:AAHqUYReFkJpMXmlRpraKmYxi72Ef-tRYxk")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", "6584789596"))
